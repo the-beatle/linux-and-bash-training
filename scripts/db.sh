81bbaf0d5e61683dce272e06f182b691a70fdc1d
the-beatle <marcae7@gmail.com>
2022-07-30 23:10:52 -0500

    empty and latin characters validators

--- scripts/db.sh
+++ scripts/db.sh
@@ -1,11 +1,34 @@
 #!/usr/bin/env bash
 
+function isALatinWord {
+	if [[ $1 =~ ^[A-Za-z_]+$ ]]; then return 0; else return 1; fi
+}
+
+function isAValidInput {
+	# VALIDATE EMPTY INPUT
+	if [[ -z $1 ]]
+	then 
+		echo "ERROR: INPUT CANNOT BE EMPTY"
+		exit 1	
+	fi
+	# VALIDATE LATIN CHARACTER
+	isALatinWord "$1"
+	if [[ $? == 1 ]]
+	then
+		echo "ERROR: ENTER ONLY LATIN CHARACTERS!"
+		exit 1
+	fi
+	echo ":VALID INPUT"
+	return 0
+}
+
 addUserToDatabase(){
-	echo "What is your user username?"
-	read username
-	echo "What is your role?"
-	read role
+	read -p "Enter User Name:" username
+	read -p "Enter Role" role	
+	isAValidInput $username
+	isAValidInput $role
 	echo "$username, $role" >> ../data/users.db
+	
 }
 
 case $1 in
